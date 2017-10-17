00d1646a532ec941ad00850c57e8026c2780aec8
Grzegorz Banach <19409@edu.p.lodz.pl>
2017-10-17 23:32:00 +0200

    initial app project structure

---
+++ app/src/androidTest/java/not_an_example/com/freelancerworld/ApplicationTest.java
@@ -0,0 +1,13 @@
+package not_an_example.com.freelancerworld;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
