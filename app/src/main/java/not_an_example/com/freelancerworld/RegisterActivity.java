2907a7c6ccf78cdce81f9840e1344f0a2c6ca779
damian <damiakuz@wp.pl>
2017-11-06 23:27:14 +0100

    cleanning after merging

--- app/src/main/java/not_an_example/com/freelancerworld/RegisterActivity.java
+++ app/src/main/java/not_an_example/com/freelancerworld/RegisterActivity.java
@@ -2,8 +2,13 @@ package not_an_example.com.freelancerworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private View mEmail;
+    private View mPass;
+    private View mRePass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
