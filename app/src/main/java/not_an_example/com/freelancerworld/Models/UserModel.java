d5eabecd2ad48987957d1988c0e3fe82d7a06bda
damian <damiakuz@wp.pl>
2017-11-06 14:30:45 +0100

    dodno modele: User i Role

---
+++ app/src/main/java/not_an_example/com/freelancerworld/Models/UserModel.java
@@ -0,0 +1,20 @@
+package not_an_example.com.freelancerworld.Models;
+
+/**
+ * Created by Damianek on 06-Nov-17.
+ */
+
+public class UserModel {
+
+    public int id;
+    public String email;
+    public String password;
+    public String name;
+    public String lastName;
+    public String active;
+    public Role[] roles;
+    public String professions;
+    public String addresses;
+    public String orderMaker;
+    public String orderTaker;
+}
