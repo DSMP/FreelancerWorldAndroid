de2adc1d1cfb88f87b1ad8f5392d3b3585e61a75
damian <damiakuz@wp.pl>
2017-11-07 15:40:49 +0100

    wykonano ResponseModel

---
+++ app/src/main/java/not_an_example/com/freelancerworld/Models/ResponseModel.java
@@ -0,0 +1,11 @@
+package not_an_example.com.freelancerworld.Models;
+
+/**
+ * Created by Damianek on 07-Nov-17.
+ */
+
+public class ResponseModel {
+    public int status;
+    public String Message;
+    public Object object;
+}
