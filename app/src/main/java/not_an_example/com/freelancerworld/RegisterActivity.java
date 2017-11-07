14d6c3d2c4fa99af8f263d35faa10cf7c73aed9a
damian <damiakuz@wp.pl>
2017-11-07 14:18:00 +0100

    parsowanie Gson obiektu i rejestracja usera

--- app/src/main/java/not_an_example/com/freelancerworld/RegisterActivity.java
+++ app/src/main/java/not_an_example/com/freelancerworld/RegisterActivity.java
@@ -7,8 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
 import java.io.StringReader;
 
+import not_an_example.com.freelancerworld.Models.UserModel;
 import not_an_example.com.freelancerworld.Utils.SendPostRequest;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -46,9 +49,12 @@ public class RegisterActivity extends AppCompatActivity {
         String surname = mSurname.getText().toString();
         if (isEmailValid(email.toString()) && isPasswordValid(pass.toString(), mRePass.getText().toString())
                 && !name.isEmpty() && !surname.isEmpty())
-        new SendPostRequest().execute("http://192.168.0.51:8080/user/register",
-                String.format("{email: %s, password: %s, name: %s, lastName: %s}",
-                                mEmail.getText(), mPass.getText(), name, surname));
+        {
+            Gson gson = new Gson();
+            UserModel user = new UserModel();
+            user.email = email; user.password = pass; user.name = name; user.lastName = surname;
+            new SendPostRequest().execute("http://192.168.0.51:8080/user/register", gson.toJson(user));
+        }
         else {
             Log.v("==========register","failed");
         }
