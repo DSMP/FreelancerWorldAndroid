ace8891229a4ac865fa48dc166984f0c44a989b9
damian <damiakuz@wp.pl>
2017-11-18 19:48:37 +0100

    zaimplementowany fragmenty aby wyswietlał dane rpzykładowe

--- app/src/main/java/not_an_example/com/freelancerworld/Fragments/UserProfileFragment.java
+++ app/src/main/java/not_an_example/com/freelancerworld/Fragments/UserProfileFragment.java
@@ -4,27 +4,26 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import not_an_example.com.freelancerworld.JobListAdapter;
 import not_an_example.com.freelancerworld.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link UserProfileFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link UserProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UserProfileFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private RecyclerView mUpperRecycler, mLowerRecycler;
+    private JobListAdapter mUpperAdapter, mLowerAdapter;
+
 
     public UserProfileFragment() {
         // Required empty public constructor
     }
+
     public static UserProfileFragment newInstance(String param1, String param2) {
         UserProfileFragment fragment = new UserProfileFragment();
         return fragment;
@@ -40,6 +39,13 @@ public class UserProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_profile, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mUpperRecycler = (RecyclerView) view.findViewById(R.id.upper_job_recycler);
+        mLowerRecycler = (RecyclerView) view.findViewById(R.id.lower_job_recycler);
+        createAdapters();
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -65,6 +71,25 @@ public class UserProfileFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
+    private void createAdapters() {
+        if ( mUpperAdapter == null) {
+            String[] upperJobs = { "Mechanic", "Hydraulik", "Programista" };
+            mUpperAdapter = new JobListAdapter(upperJobs);
+        }
+
+        if ( mLowerAdapter == null) {
+            String[] lowerJobs = { "Zlecenie 11", "Zlecenie 13", "Zlecenie 21", "Zlecenie 24" };
+            mLowerAdapter = new JobListAdapter(lowerJobs);
+        }
+
+        mUpperRecycler.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        mLowerRecycler.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        mUpperRecycler.setAdapter(mUpperAdapter);
+        mLowerRecycler.setAdapter(mLowerAdapter);
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
