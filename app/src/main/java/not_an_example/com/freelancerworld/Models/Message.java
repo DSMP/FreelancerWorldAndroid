ef7f8dda3e34f51358dfb1ae533a8e97838dc337
damian <damiakuz@wp.pl>
2017-11-18 15:59:13 +0100

    dodano model Message

---
+++ app/src/main/java/not_an_example/com/freelancerworld/Models/Message.java
@@ -0,0 +1,10 @@
+package not_an_example.com.freelancerworld.Models;
+
+/**
+ * Created by Damianek on 18-Nov-17.
+ */
+
+public class Message {
+    public int status;
+    public String message;
+}
