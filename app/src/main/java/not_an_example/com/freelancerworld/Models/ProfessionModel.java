92cae3308dd1ff87ce72a9a1f120f5b270da2d93
damian <damiakuz@wp.pl>
2017-11-18 17:22:30 +0100

    utworzono model professji z refaktoryzacja obiektu profession

--- app/src/main/java/not_an_example/com/freelancerworld/Models/ProfessionModel.java
+++ app/src/main/java/not_an_example/com/freelancerworld/Models/ProfessionModel.java
@@ -1,9 +1,13 @@
 package not_an_example.com.freelancerworld.Models;
 
+import not_an_example.com.freelancerworld.Models.SmallModels.User;
+import not_an_example.com.freelancerworld.Models.SmallModels.Profession;
+
 /**
  * Created by Damianek on 18-Nov-17.
  */
 
 public class ProfessionModel {
-    public
+    public User user;
+    public Profession profession;
 }
--- app/src/main/java/not_an_example/com/freelancerworld/Models/SmallModels/Profession.java
+++ app/src/main/java/not_an_example/com/freelancerworld/Models/SmallModels/Profession.java
@@ -4,6 +4,6 @@ package not_an_example.com.freelancerworld.Models.SmallModels;
  * Created by Damianek on 18-Nov-17.
  */
 
-public class profession {
+public class Profession {
     public String name;
 }
