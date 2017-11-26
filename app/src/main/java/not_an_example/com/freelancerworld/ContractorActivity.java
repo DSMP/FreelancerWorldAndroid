32cbd4da9cd211bbf384b6b3a27143dbdfb8dfc7
damian <damiakuz@wp.pl>
2017-11-26 12:01:35 +0100

    zainicjalizowano itemy z layouty w activity

--- app/src/main/java/not_an_example/com/freelancerworld/ContractorActivity.java
+++ app/src/main/java/not_an_example/com/freelancerworld/ContractorActivity.java
@@ -2,12 +2,25 @@ package not_an_example.com.freelancerworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ContractorActivity extends AppCompatActivity {
+
+    TextView contrFullName;
+    TextView contrPhoneNumber;
+    TextView contrProfessions;
+    Button acceptContractor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contractor);
+        contrFullName = (TextView) findViewById(R.id.ContractorName);
+        contrPhoneNumber = (TextView) findViewById(R.id.ContractorPhoneNumber);
+        contrProfessions = (TextView) findViewById(R.id.ContractorProfessions);
+        acceptContractor = (Button) findViewById(R.id.ContractorAcceptButton);
+        
+
     }
 }
