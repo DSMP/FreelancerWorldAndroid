25dd133eeb3ca31d570d7d2979675ed6e694ce9d
damian <damiakuz@wp.pl>
2017-11-26 11:47:58 +0100

    stworzony layout

---
+++ app/src/main/java/not_an_example/com/freelancerworld/ContractorActivity.java
@@ -0,0 +1,13 @@
+package not_an_example.com.freelancerworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContractorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contractor);
+    }
+}
